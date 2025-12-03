2f6729e767fe8ccb1ea4e8e96198296f03aa7817
bjornpetersson1 <bjornpetersson1@live.se>
2025-12-03 23:25:07 +0100

    Del 2 löst

--- AOC_2025_3_Dec/Program.cs
+++ AOC_2025_3_Dec/Program.cs
@@ -42,7 +42,7 @@ for (int i = 0; i < inputs.Length; i++)
     string rest = inputs[i]; 
     for (global::System.Int32 j = 9; j > 0 && !joltageDone; j--)
     {
-        if (rest.Contains((char)('0' + j)) && inputs[i].IndexOf((char)('0' + j)) < inputs[i].Length - 11)
+        if (rest.Contains((char)('0' + j)) && rest.IndexOf((char)('0' + j)) < rest.Length - 11)
         {
             joltage += (char)('0' + j);
             int Index = rest.IndexOf((char)('0' + j));
@@ -50,7 +50,7 @@ for (int i = 0; i < inputs.Length; i++)
 
             for (global::System.Int32 k = 9; k >= 0 && !joltageDone; k--)
             {
-                if (rest.Contains((char)('0' + k)) && inputs[i].IndexOf((char)('0' + j)) < inputs[i].Length - 10)
+                if (rest.Contains((char)('0' + k)) && rest.IndexOf((char)('0' + k)) < rest.Length - 10)
                 {
                     joltage += (char)('0' + k);
                     Index = rest.IndexOf((char)('0' + k));
@@ -58,7 +58,7 @@ for (int i = 0; i < inputs.Length; i++)
 
                     for (global::System.Int32 l = 9; l >= 0 && !joltageDone; l--)
                     {
-                        if (rest.Contains((char)('0' + l)) && inputs[i].IndexOf((char)('0' + j)) < inputs[i].Length - 9)
+                        if (rest.Contains((char)('0' + l)) && rest.IndexOf((char)('0' + l)) < rest.Length - 9)
                         {
                             joltage += (char)('0' + l);
                             Index = rest.IndexOf((char)('0' + l));
@@ -66,7 +66,7 @@ for (int i = 0; i < inputs.Length; i++)
 
                             for (global::System.Int32 m = 9; m >= 0 && !joltageDone; m--)
                             {
-                                if (rest.Contains((char)('0' + m)) && inputs[i].IndexOf((char)('0' + j)) < inputs[i].Length - 8)
+                                if (rest.Contains((char)('0' + m)) && rest.IndexOf((char)('0' + m)) < rest.Length - 8)
                                 {
                                     joltage += (char)('0' + m);
                                     Index = rest.IndexOf((char)('0' + m));
@@ -74,7 +74,7 @@ for (int i = 0; i < inputs.Length; i++)
 
                                     for (global::System.Int32 n = 9; n >= 0 && !joltageDone; n--)
                                     {
-                                        if (rest.Contains((char)('0' + n)) && inputs[i].IndexOf((char)('0' + j)) < inputs[i].Length - 7)
+                                        if (rest.Contains((char)('0' + n)) && rest.IndexOf((char)('0' + n)) < rest.Length - 7)
                                         {
                                             joltage += (char)('0' + n);
                                             Index = rest.IndexOf((char)('0' + n));
@@ -82,7 +82,7 @@ for (int i = 0; i < inputs.Length; i++)
 
                                             for (global::System.Int32 o = 9; o >= 0 && !joltageDone; o--)
                                             {
-                                                if (rest.Contains((char)('0' + o)) && inputs[i].IndexOf((char)('0' + j)) < inputs[i].Length - 6)
+                                                if (rest.Contains((char)('0' + o)) && rest.IndexOf((char)('0' + o)) < rest.Length - 6)
                                                 {
                                                     joltage += (char)('0' + o);
                                                     Index = rest.IndexOf((char)('0' + o));
@@ -90,7 +90,7 @@ for (int i = 0; i < inputs.Length; i++)
 
                                                     for (global::System.Int32 p = 9; p >= 0 && !joltageDone; p--)
                                                     {
-                                                        if (rest.Contains((char)('0' + p)) && inputs[i].IndexOf((char)('0' + j)) < inputs[i].Length - 5)
+                                                        if (rest.Contains((char)('0' + p)) && rest.IndexOf((char)('0' + p)) < rest.Length - 5)
                                                         {
                                                             joltage += (char)('0' + p);
                                                             Index = rest.IndexOf((char)('0' + p));
@@ -98,7 +98,7 @@ for (int i = 0; i < inputs.Length; i++)
 
                                                             for (global::System.Int32 q = 9; q >= 0 && !joltageDone; q--)
                                                             {
-                                                                if (rest.Contains((char)('0' + q)) && inputs[i].IndexOf((char)('0' + j)) < inputs[i].Length - 4)
+                                                                if (rest.Contains((char)('0' + q)) && rest.IndexOf((char)('0' + q)) < rest.Length - 4)
                                                                 {
                                                                     joltage += (char)('0' + q);
                                                                     Index = rest.IndexOf((char)('0' + q));
@@ -106,7 +106,7 @@ for (int i = 0; i < inputs.Length; i++)
 
                                                                     for (global::System.Int32 r = 9; r >= 0 && !joltageDone; r--)
                                                                     {
-                                                                        if (rest.Contains((char)('0' + r)) && inputs[i].IndexOf((char)('0' + j)) < inputs[i].Length - 3)
+                                                                        if (rest.Contains((char)('0' + r)) && rest.IndexOf((char)('0' + r)) < rest.Length - 3)
                                                                         {
                                                                             joltage += (char)('0' + r);
                                                                             Index = rest.IndexOf((char)('0' + r));
@@ -114,7 +114,7 @@ for (int i = 0; i < inputs.Length; i++)
 
                                                                             for (global::System.Int32 s = 9; s >= 0 && !joltageDone; s--)
                                                                             {
-                                                                                if (rest.Contains((char)('0' + s)) && inputs[i].IndexOf((char)('0' + j)) < inputs[i].Length - 2)
+                                                                                if (rest.Contains((char)('0' + s)) && rest.IndexOf((char)('0' + s)) < rest.Length - 2)
                                                                                 {
                                                                                     joltage += (char)('0' + s);
                                                                                     Index = rest.IndexOf((char)('0' + s));
@@ -122,7 +122,7 @@ for (int i = 0; i < inputs.Length; i++)
 
                                                                                     for (global::System.Int32 t = 9; t >= 0 && !joltageDone; t--)
                                                                                     {
-                                                                                        if (rest.Contains((char)('0' + t)) && inputs[i].IndexOf((char)('0' + j)) < inputs[i].Length - 1)
+                                                                                        if (rest.Contains((char)('0' + t)) && rest.IndexOf((char)('0' + t)) < rest.Length - 1)
                                                                                         {
                                                                                             joltage += (char)('0' + t);
                                                                                             Index = rest.IndexOf((char)('0' + t));
