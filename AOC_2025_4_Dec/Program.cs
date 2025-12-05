e9aacc79fb21c0f10e72d8148561718e545b00c1
bjornpetersson1 <bjornpetersson1@live.se>
2025-12-05 10:26:23 +0100

    4 dec del 2 lösning

--- AOC_2025_4_Dec/Program.cs
+++ AOC_2025_4_Dec/Program.cs
@@ -1,5 +1,113 @@
 ﻿using AOC_2025_4_Dec;
 
+//Del 1
+//string[] inputRows = InputData.input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+//int rowsCount = inputRows.Length;
+//int colsCount = inputRows[0].Length;
+
+//char[,] inputRowsAndColums = new char[rowsCount, colsCount];
+
+//for (int i = 0; i < rowsCount; i++)
+//{
+//    for (int j = 0; j < colsCount; j++)
+//    {
+//        inputRowsAndColums[i, j] = inputRows[i][j];
+//    }
+//}
+//int accessablePapers = 0;
+//for (int i = 0; i < rowsCount; i++)
+//{
+//    for (global::System.Int32 j = 0; j < colsCount; j++)
+//    {
+//        if (inputRowsAndColums[i, j] != '@') continue;
+//        int paperCount = 0;
+//        if (i == 0 && j == 0)
+//        {
+//            if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
+//            for (global::System.Int32 k = 0; k < 2; k++)
+//            {
+//                if (inputRowsAndColums[i + 1, j + k] == '@') paperCount++;
+//            }
+//        }
+//        else if (i == 0 && j == colsCount - 1)
+//        {
+//            if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
+//            for (global::System.Int32 k = 0; k < 2; k++)
+//            {
+//                if (inputRowsAndColums[i + 1, j - 1 + k] == '@') paperCount++;
+//            }
+//        }
+//        else if (i == rowsCount - 1 && j == 0)
+//        {
+//            if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
+//            for (global::System.Int32 k = 0; k < 2; k++)
+//            {
+//                if (inputRowsAndColums[i - 1, j + k] == '@') paperCount++;
+//            }
+//        }
+//        else if (i == rowsCount - 1 && j == colsCount - 1)
+//        {
+//            if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
+//            for (global::System.Int32 k = 0; k < 2; k++)
+//            {
+//                if (inputRowsAndColums[i - 1, j - 1 + k] == '@') paperCount++;
+//            }
+//        }
+//        else if (i == 0)
+//        {
+//            if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
+//            if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
+//            for (global::System.Int32 k = 0; k < 3; k++)
+//            {
+//                if (inputRowsAndColums[i + 1, j - 1 + k] == '@') paperCount++;
+//            }
+//        }
+//        else if (i == rowsCount -1)
+//        {
+//            if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
+//            if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
+//            for (global::System.Int32 k = 0; k < 3; k++)
+//            {
+//                if (inputRowsAndColums[i - 1, j - 1 + k] == '@') paperCount++;
+//            }
+//        }
+//        else if (j == 0)
+//        {
+//            if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
+//            for (global::System.Int32 k = 0; k < 2; k++)
+//            {
+//                if (inputRowsAndColums[i - 1, j + k] == '@') paperCount++;
+//                if (inputRowsAndColums[i + 1, j + k] == '@') paperCount++;
+//            }
+//        }
+//        else if (j == colsCount - 1)
+//        {
+//            if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
+//            for (global::System.Int32 k = 0; k < 2; k++)
+//            {
+//                if (inputRowsAndColums[i - 1, j - k] == '@') paperCount++;
+//                if (inputRowsAndColums[i + 1, j - k] == '@') paperCount++;
+//            }
+//        }
+//        else
+//        {
+//            if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
+//            if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
+//            for (global::System.Int32 k = 0; k < 3; k++)
+//            {
+//                if (inputRowsAndColums[i - 1, j - 1 + k] == '@') paperCount++;
+//                if (inputRowsAndColums[i + 1, j - 1 + k] == '@') paperCount++;
+//            }
+//        }
+//        if (paperCount < 4) accessablePapers++;
+//    }
+//}
+
+//Console.WriteLine(accessablePapers);
+
+//del 2
+
 string[] inputRows = InputData.input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
 
 int rowsCount = inputRows.Length;
@@ -15,92 +123,103 @@ for (int i = 0; i < rowsCount; i++)
     }
 }
 int accessablePapers = 0;
-for (int i = 0; i < rowsCount; i++)
+int accPapThisRound = 0;
+do
 {
-    for (global::System.Int32 j = 0; j < colsCount; j++)
+    accPapThisRound = 0;
+    for (int i = 0; i < rowsCount; i++)
     {
-        if (inputRowsAndColums[i, j] != '@') continue;
-        int paperCount = 0;
-        if (i == 0 && j == 0)
+        for (global::System.Int32 j = 0; j < colsCount; j++)
         {
-            if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
-            for (global::System.Int32 k = 0; k < 2; k++)
+            if (inputRowsAndColums[i, j] != '@') continue;
+            int paperCount = 0;
+            if (i == 0 && j == 0)
             {
-                if (inputRowsAndColums[i + 1, j + k] == '@') paperCount++;
+                if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
+                for (global::System.Int32 k = 0; k < 2; k++)
+                {
+                    if (inputRowsAndColums[i + 1, j + k] == '@') paperCount++;
+                }
+            }
+            else if (i == 0 && j == colsCount - 1)
+            {
+                if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
+                for (global::System.Int32 k = 0; k < 2; k++)
+                {
+                    if (inputRowsAndColums[i + 1, j - 1 + k] == '@') paperCount++;
+                }
+            }
+            else if (i == rowsCount - 1 && j == 0)
+            {
+                if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
+                for (global::System.Int32 k = 0; k < 2; k++)
+                {
+                    if (inputRowsAndColums[i - 1, j + k] == '@') paperCount++;
+                }
+            }
+            else if (i == rowsCount - 1 && j == colsCount - 1)
+            {
+                if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
+                for (global::System.Int32 k = 0; k < 2; k++)
+                {
+                    if (inputRowsAndColums[i - 1, j - 1 + k] == '@') paperCount++;
+                }
+            }
+            else if (i == 0)
+            {
+                if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
+                if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
+                for (global::System.Int32 k = 0; k < 3; k++)
+                {
+                    if (inputRowsAndColums[i + 1, j - 1 + k] == '@') paperCount++;
+                }
+            }
+            else if (i == rowsCount - 1)
+            {
+                if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
+                if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
+                for (global::System.Int32 k = 0; k < 3; k++)
+                {
+                    if (inputRowsAndColums[i - 1, j - 1 + k] == '@') paperCount++;
+                }
+            }
+            else if (j == 0)
+            {
+                if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
+                for (global::System.Int32 k = 0; k < 2; k++)
+                {
+                    if (inputRowsAndColums[i - 1, j + k] == '@') paperCount++;
+                    if (inputRowsAndColums[i + 1, j + k] == '@') paperCount++;
+                }
+            }
+            else if (j == colsCount - 1)
+            {
+                if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
+                for (global::System.Int32 k = 0; k < 2; k++)
+                {
+                    if (inputRowsAndColums[i - 1, j - k] == '@') paperCount++;
+                    if (inputRowsAndColums[i + 1, j - k] == '@') paperCount++;
+                }
+            }
+            else
+            {
+                if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
+                if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
+                for (global::System.Int32 k = 0; k < 3; k++)
+                {
+                    if (inputRowsAndColums[i - 1, j - 1 + k] == '@') paperCount++;
+                    if (inputRowsAndColums[i + 1, j - 1 + k] == '@') paperCount++;
+                }
+            }
+            if (paperCount < 4)
+            {
+                accessablePapers++;
+                accPapThisRound++;
+                inputRowsAndColums[i, j] = '.';
             }
         }
-        else if (i == 0 && j == colsCount - 1)
-        {
-            if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
-            for (global::System.Int32 k = 0; k < 2; k++)
-            {
-                if (inputRowsAndColums[i + 1, j - 1 + k] == '@') paperCount++;
-            }
-        }
-        else if (i == rowsCount - 1 && j == 0)
-        {
-            if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
-            for (global::System.Int32 k = 0; k < 2; k++)
-            {
-                if (inputRowsAndColums[i - 1, j + k] == '@') paperCount++;
-            }
-        }
-        else if (i == rowsCount - 1 && j == colsCount - 1)
-        {
-            if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
-            for (global::System.Int32 k = 0; k < 2; k++)
-            {
-                if (inputRowsAndColums[i - 1, j - 1 + k] == '@') paperCount++;
-            }
-        }
-        else if (i == 0)
-        {
-            if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
-            if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
-            for (global::System.Int32 k = 0; k < 3; k++)
-            {
-                if (inputRowsAndColums[i + 1, j - 1 + k] == '@') paperCount++;
-            }
-        }
-        else if (i == rowsCount -1)
-        {
-            if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
-            if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
-            for (global::System.Int32 k = 0; k < 3; k++)
-            {
-                if (inputRowsAndColums[i - 1, j - 1 + k] == '@') paperCount++;
-            }
-        }
-        else if (j == 0)
-        {
-            if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
-            for (global::System.Int32 k = 0; k < 2; k++)
-            {
-                if (inputRowsAndColums[i - 1, j + k] == '@') paperCount++;
-                if (inputRowsAndColums[i + 1, j + k] == '@') paperCount++;
-            }
-        }
-        else if (j == colsCount - 1)
-        {
-            if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
-            for (global::System.Int32 k = 0; k < 2; k++)
-            {
-                if (inputRowsAndColums[i - 1, j - k] == '@') paperCount++;
-                if (inputRowsAndColums[i + 1, j - k] == '@') paperCount++;
-            }
-        }
-        else
-        {
-            if (inputRowsAndColums[i, j - 1] == '@') paperCount++;
-            if (inputRowsAndColums[i, j + 1] == '@') paperCount++;
-            for (global::System.Int32 k = 0; k < 3; k++)
-            {
-                if (inputRowsAndColums[i - 1, j - 1 + k] == '@') paperCount++;
-                if (inputRowsAndColums[i + 1, j - 1 + k] == '@') paperCount++;
-            }
-        }
-        if (paperCount < 4) accessablePapers++;
     }
 }
+while (accPapThisRound != 0);
 
 Console.WriteLine(accessablePapers);
