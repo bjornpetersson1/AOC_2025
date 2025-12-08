7218eb21389157fbeb722313ff1ecf5ec47182e7
bjornpetersson1 <bjornpetersson1@live.se>
2025-12-08 13:59:55 +0100

    förberett inputdata 6 dec

---
+++ AOC_2025_6_Dec/Program.cs
@@ -0,0 +1,14 @@
+﻿using AOC_2025_6_Dec;
+using System.Xml;
+//del 1
+string[] rowsOfNumbers = InputData.numbers.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+//string[] numbers = InputData.numbers.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+string[] operators = InputData.operators.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+List<string> sepOperators = new List<string>();
+
+foreach (var r in rowsOfNumbers)
+{
+    
+}
+
+Console.WriteLine();
