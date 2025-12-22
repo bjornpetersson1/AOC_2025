616157eb30cef53e4ea28b6367e828b186c94294
bjornpetersson1 <bjornpetersson1@live.se>
2025-12-22 11:06:13 +0100

    Organiserat input för 8 dec

---
+++ AOC_2025_8_Dec/Program.cs
@@ -0,0 +1,16 @@
+﻿using AOC_2025_8_Dec;
+
+List<string> stringPoints = (InputData.inputPoints.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)).ToList();
+var points = new HashSet<(int x, int y, int z)>();
+
+foreach (var s in stringPoints)
+{
+    var coOrds = s.Split(',');
+    
+    int x = int.Parse(coOrds[0]);
+    int y = int.Parse(coOrds[1]);
+    int z = int.Parse(coOrds[2]);
+
+    points.Add((x, y, z));
+}
+Console.WriteLine();
