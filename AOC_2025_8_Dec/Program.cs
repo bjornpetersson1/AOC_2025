47c015508b1d94c4cb70baed1dbef2a24cc10042
bjornpetersson1 <bjornpetersson1@live.se>
2025-12-23 11:49:41 +0100

    En typ av lösning på del 1 som är fel :)

--- AOC_2025_8_Dec/Program.cs
+++ AOC_2025_8_Dec/Program.cs
@@ -1,5 +1,8 @@
 ﻿using AOC_2025_8_Dec;
+using System.Drawing;
 //räkna ut avståndet, sortera, koppla ihop, multiplicera top 3 grupper
+
+    //allt hamnar i samma grupp nu.. måste läsa igenom uppgiften igen, måste ha missat någon parameter
 
 List<string> stringPoints = (InputData.inputPoints.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)).ToList();
 var points = new HashSet<(float x, float y, float z)>();
@@ -14,7 +17,7 @@ foreach (var s in stringPoints)
 
     points.Add((x, y, z));
 }
-var results = new HashSet<((float x, float y, float z) p1, (float x, float y, float z) p2, double distance, int group)>();
+var results = new HashSet<((float x, float y, float z) p1, (float x, float y, float z) p2, double distance)>();
 
 foreach (var p1 in points)
 {
@@ -23,11 +26,66 @@ foreach (var p1 in points)
         double distance = Math.Sqrt(Math.Pow((p1.x - p2.x), 2) + Math.Pow((p1.y - p2.y), 2) + Math.Pow((p1.z - p2.z), 2));
         if (distance != 0)
         {
-            results.Add((p1, p2, distance, 0));
+            results.Add((p1, p2, distance));
         }
     }
 }
 
 var sortedPoints = results.OrderBy(r => r.distance).ToList();
-//hitta ett sätt att gruppera :) tänk på om båda punkterna tillhör varsin grupp
+var groupedPoints = new Dictionary<(float x, float y, float z), int>();
+int group = 1;
+foreach (var s in sortedPoints)
+{
+    if(groupedPoints.TryGetValue(s.p1, out int point1Group) && !groupedPoints.TryGetValue(s.p2, out int point2Group))
+    {
+        groupedPoints.Add(s.p2, point1Group);
+    }
+    else if (!groupedPoints.TryGetValue(s.p1, out int point3Group) && groupedPoints.TryGetValue(s.p2, out int point4Group))
+    {
+        groupedPoints.Add(s.p1, point4Group);
+    }
+    else if (groupedPoints.TryGetValue(s.p1, out int point5Group) && groupedPoints.TryGetValue(s.p2, out int point6Group))
+    {
+        if (point5Group != point6Group)
+        {
+            int size1 = CountGroupMembers(point5Group);
+            int size2 = CountGroupMembers(point6Group);
+
+            int targetGroup;
+            int sourceGroup;
+
+            if (size1 >= size2)
+            {
+                targetGroup = point5Group;   
+                sourceGroup = point6Group;   
+            }
+            else
+            {
+                targetGroup = point6Group;
+                sourceGroup = point5Group;
+            }
+
+            var keysToMove = groupedPoints
+                .Where(p => p.Value == sourceGroup)
+                .Select(p => p.Key)
+                .ToList();
+
+            foreach (var key in keysToMove)
+            {
+                groupedPoints[key] = targetGroup;
+            }
+        }
+    }
+    else
+    {
+        groupedPoints.Add(s.p1, group);
+        groupedPoints.Add(s.p2, group);
+        group++;
+    }
+}
 Console.WriteLine();
+
+int CountGroupMembers(int groupId)
+{
+    return groupedPoints.Count(p => p.Value == groupId);
+}
